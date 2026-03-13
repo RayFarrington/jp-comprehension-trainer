2d1e6dd299ff7b1cb79b7d5fac6cafc5ba4e6bde
RayFarrington <farringtonchandler@gmail.com>
2026-03-13 13:04:40 -0700

    Hiragana and Kanji

--- logic.py
+++ logic.py
@@ -4,7 +4,11 @@ from convert_numbers_to_japanese import Convert, ConvertKanji
 def generate_number():
     return randint(0, 1000000)
 
-def get_message():
+def get_message(opt) -> str:
+    if (opt is "audio"):
+        return "not yet implimented"
+    elif opt != "hiragana" and opt != "kanji":
+        return "Error: invalid option in get_message"
     num = randint(0, 1000000)
-    hiragana = Convert(num, "hiragana")
-    return hiragana
+    str = Convert(num, opt)
+    return str
--- ui.py
+++ ui.py
@@ -4,21 +4,27 @@ from logic import get_message
 class AppUI:
     def __init__(self):
         self.root = tk.Tk()
-        self.root.title("My App")
+        self.root.title("Language Comprehension Training")
         self.root.geometry("300x150")
 
         self.label = tk.Label(self.root, text="Press the button")
         self.label.pack(pady=10)
 
-        self.button = tk.Button(
+        self.hiragana_button = tk.Button(
             self.root,
-            text="Click",
-            command=self.on_click
+            text="Hiragana",
+            command=lambda: self.on_click("hiragana")
         )
-        self.button.pack()
+        self.hiragana_button.pack()
+        self.kanji_button = tk.Button(
+                self.root,
+                text="Kanji",
+                command=lambda: self.on_click("kanji")
+                )
+        self.kanji_button.pack()
 
-    def on_click(self):
-        message = get_message()
+    def on_click(self, opt):
+        message = get_message(opt)
         self.label.config(text=message)
 
     def run(self):
